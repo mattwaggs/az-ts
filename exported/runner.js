af1559f1dfe865788c3b493d7f2835b727a9d62e
Matt Waggoner <matt.waggoner@devfacto.com>
2020-10-02 12:01:21 -0600

    version bump

--- exported/runner.js
+++ exported/runner.js
@@ -1,8 +1,10 @@
-import shelljs from "shelljs";
-import fs from "fs";
+#!/usr/bin/env node
+
+var shelljs = require("shelljs");
+var fs = require("fs");
 
 var args = process.argv.slice(2);
-const filePath = args[0];
+var filePath = args[0];
 
 if (!fs.existsSync(filePath)) {
   throw new Error("File could not be found");
